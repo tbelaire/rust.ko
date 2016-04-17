d2397753a4f04d36b0a4381f74e11918c01331ba
Theo Belaire <theo.belaire@gmail.com>
2016-04-17 12:31:23 -0400

    Moved the rest of the logic into Rust

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ static NUMBER_OPENS: AtomicUsize = ATOMIC_USIZE_INIT;
 // This is unsafe, please make a re-entraint version.
 static mut message: [u8; 256] = [0; 256];
 static mut size_of_message: usize = 0;
+static mut major_number: c_int = 0;
 static mut fops: kernel::Struct_file_operations =
     kernel::Struct_file_operations {
         read: Some(rust_dev_read),
@@ -48,16 +49,64 @@ static mut fops: kernel::Struct_file_operations =
         show_fdinfo: None,
     };
 
+// No CStr, so manually null terminate these strings :/
 const DEVICE_NAME: &'static str = "erchar\0";
 const CLASS_NAME: &'static str = "er\0";
 
+static mut erchar_class: *mut kernel::Struct_class = core::ptr::null_mut();
+static mut erchar_device: *mut kernel::Struct_device = core::ptr::null_mut();
+
 #[no_mangle]
 pub unsafe extern "C" fn rust_main() -> c_int {
-    // println!("Hello {} from {} Rust!++", 42, 0);
+    println!("rot13-rust: initializing");
     NUMBER_OPENS.store(0, Ordering::SeqCst);
-    let major_number =
-        my_register_chrdev(0, DEVICE_NAME.as_ptr() as *const c_char, &fops);
-    major_number
+    major_number = my_register_chrdev(0,
+                                      DEVICE_NAME.as_ptr() as *const c_char,
+                                      &fops);
+    if major_number < 0 {
+        // KERN_ALERT
+        println!("rot13-rust: failed to register a major number");
+        return major_number;
+    }
+    // KERN_INFO
+    println!("rot13-rust: registered correctly with major number {}",
+             major_number);
+
+    erchar_class = my_class_create(CLASS_NAME.as_ptr() as *const c_char);
+    if IS_ERR_VALUE(erchar_class as c_long) {
+        my_unregister_chrdev(major_number as c_uint,
+                             DEVICE_NAME.as_ptr() as *const c_char);
+        println!("rot13-rust: failed to register device class");
+        return PTR_ERR(erchar_class) as c_int;
+    }
+    println!("rot13-rust: created class correctly");
+    erchar_device =
+        kernel::device_create(erchar_class,
+                              core::ptr::null_mut(),
+                              my_MKDEV(major_number, 0),
+                              core::ptr::null_mut(),
+                              DEVICE_NAME.as_ptr() as *const c_char);
+
+    if (erchar_device as usize) < 0 {
+        kernel::class_unregister(erchar_class); // ???
+        kernel::class_destroy(erchar_class);
+        my_unregister_chrdev(major_number as c_uint,
+                             DEVICE_NAME.as_ptr() as *const c_char);
+        println!("rot13-rust: failed to register device class\n");
+        return PTR_ERR(erchar_device) as c_int;
+    }
+    println!("rot13-rust: registered device correctly");
+    0
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn rust_exit() {
+    println!("rot13-rust: exit");
+    kernel::device_destroy(erchar_class, my_MKDEV(major_number, 0));
+    kernel::class_unregister(erchar_class); // ???
+    kernel::class_destroy(erchar_class);
+    my_unregister_chrdev(major_number as c_uint,
+                         DEVICE_NAME.as_ptr() as *const c_char);
 }
 
 
@@ -69,7 +118,7 @@ pub unsafe extern "C" fn rust_dev_open(_inode: *mut kernel::Struct_inode,
                                        _file: *mut kernel::Struct_file)
                                        -> c_int {
     let old_num_opens = NUMBER_OPENS.fetch_add(1, Ordering::SeqCst);
-    println!("ERChar Device has been opened {} times before.",
+    println!("rot13-rust: Device has been opened {} times before.",
              old_num_opens);
     0
 }
@@ -78,7 +127,7 @@ pub unsafe extern "C" fn rust_dev_open(_inode: *mut kernel::Struct_inode,
 pub unsafe extern "C" fn rust_dev_release(_inode: *mut kernel::Struct_inode,
                                           _file: *mut kernel::Struct_file)
                                           -> c_int {
-    println!("ERChar Device successfully closed");
+    println!("rot13-rust: Device successfully closed");
     0
 }
 
@@ -95,11 +144,12 @@ pub unsafe extern "C" fn rust_dev_read(_file: *mut kernel::Struct_file,
                               (&message) as *const u8 as *const c_void,
                               size_of_message as c_uint);
     if error_count == 0 {
-        println!("ERchar: Sent {} characters to the user.", size_of_message);
+        println!("rot13-rust:: Sent {} characters to the user.",
+                 size_of_message);
         size_of_message = 0;
         return 0;
     } else {
-        println!("ERchar: Failed to send {} characters to the user.",
+        println!("rot13-rust:: Failed to send {} characters to the user.",
                  error_count);
         return -(std::os::errors::ERROR::EFAULT as kernel::ssize_t);
     }
@@ -133,7 +183,7 @@ pub unsafe extern "C" fn rust_dev_write(_file: *mut kernel::Struct_file,
         message[i] = rot_13(*buffer.offset(i as isize) as u8);
     }
     size_of_message = len as usize;
-    println!("ERChar: Rotated {} characters.", len);
+    println!("rot13-rust: Rotated {} characters.", len);
     return len as kernel::ssize_t;
 }
 
@@ -143,4 +193,19 @@ unsafe fn my_register_chrdev(major: c_uint,
                              fops_: &kernel::Struct_file_operations)
                              -> c_int {
     kernel::__register_chrdev(major, 0, 256, name, fops_ as *const _)
+}
+unsafe fn my_unregister_chrdev(major: c_uint, name: *const c_char) {
+    kernel::__unregister_chrdev(major, 0, 256, name);
+}
+
+extern "C" {
+    fn my_class_create(name: *const c_char) -> *mut kernel::Struct_class;
+    fn my_MKDEV(major: c_int, minor: c_int) -> c_uint;
+}
+
+fn IS_ERR_VALUE(x: c_long) -> bool {
+    x >= (-4095 as c_long)
+}
+fn PTR_ERR<T>(ptr: *const T) -> c_long {
+    ptr as c_long
 }
--- src/module.c
+++ src/module.c
@@ -16,11 +16,6 @@ MODULE_AUTHOR("Theo Belaire");
 MODULE_DESCRIPTION("A simple Linux char device for exploring Rust");
 MODULE_VERSION("0.1");
 
-static int majorNumber;
-static struct class* ercharClass = NULL;
-static struct device* ercharDevice = NULL;
-
-
 char __morestack[1024];
 char _GLOBAL_OFFSET_TABLE_;
 
@@ -29,60 +24,24 @@ void abort(void)
     BUG();
 }
 
-long my_copy_to_user(void __user *to,
-        const void* from, unsigned long n)
-{
-    return copy_to_user(to, from, n);
+unsigned int my_MKDEV(int major, int minor) {
+    return MKDEV(major, minor);
 }
-int my_register_chrdev(unsigned int major, const char *name,
-        const struct file_operations *fops)
-{
-    return __register_chrdev(major, 0, 256, name, fops);
+
+struct class * my_class_create(char* name) {
+    return class_create(THIS_MODULE, name);
 }
 extern int rust_main(void);
+extern void rust_exit(void);
 
 static int hello_init(void)
 {
-    printk(KERN_INFO "rot13-rust: init\n");
-    // majorNumber = register_chrdev(0, DEVICE_NAME, &fops);
-    majorNumber = rust_main();
-
-    if (majorNumber < 0) {
-        printk(KERN_ALERT "rot13-rust: failed to register a major number\n");
-        return majorNumber;
-    }
-    printk(KERN_INFO "rot13-rust: registered correctly with major number %d\n", majorNumber);
-
-    ercharClass = class_create(THIS_MODULE, CLASS_NAME);
-    if (IS_ERR(ercharClass)){
-        unregister_chrdev(majorNumber, DEVICE_NAME);
-        printk(KERN_ALERT "rot13-rust: failed to register device class\n");
-        return PTR_ERR(ercharClass);
-    }
-    printk(KERN_INFO "rot13-rust: created class correctly\n");
-
-    ercharDevice = device_create(ercharClass, NULL, MKDEV(majorNumber, 0),
-            NULL, DEVICE_NAME);
-
-    if (ercharDevice < 0) {
-        class_unregister(ercharClass); // ???
-        class_destroy(ercharClass);
-        unregister_chrdev(majorNumber, DEVICE_NAME);
-        printk(KERN_ALERT "rot13-rust: failed to register device class\n");
-        return PTR_ERR(ercharDevice);
-    }
-
-    printk(KERN_INFO "rot13-rust: Device created successfully\n");
-    return 0;
+    return rust_main();
 }
 
 static void hello_exit(void)
 {
-    printk(KERN_INFO "rot13-rust: exit\n");
-    device_destroy(ercharClass, MKDEV(majorNumber, 0));
-    class_unregister(ercharClass);
-    class_destroy(ercharClass);
-    unregister_chrdev(majorNumber, DEVICE_NAME);
+    rust_exit();
 }
 
 module_init(hello_init);
